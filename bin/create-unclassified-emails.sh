fa3f873c699c26d4d53db614cf5c4868b4b3eaf5
Dean Wampler <dean@deanwampler.com>
2014-03-27 20:34:03 -0500

    Saving work on exercises.

---
+++ bin/create-unclassified-emails.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Used to extract 10% of the Enron emails for "reclassifying"
+
+dir=$(dirname $0)
+root=$(dirname $dir)
+
+mkdir -p $root/data/enron_spam_ham/unclassified
+
+let RANGE=100
+let PERCENT=10
+
+rand() {
+  number=$RANDOM
+  let "number %= $RANGE"
+  echo $number
+}
+
+let i2=0
+find $root/data/enron_spam_ham/{spam,ham} -type f | while read f
+do
+  let n=$(rand)
+  if [ $n -lt $PERCENT ]
+  then 
+    let i2=$i2+1
+    cp $f $root/data/enron_spam_ham/unclassified
+  fi
+done
+
+echo "$i2 files copied"
+
--- exercises/lda-enron-email.sh
+++
@@ -1,2 +0,0 @@
-export FACTORIE_HOME=/Users/deanwampler/projects/data/factorie/factorie-git
-$FACTORIE_HOME/bin/fac lda --read-dirs data/shakespeare --num-topics 20 --num-iterations 100 
